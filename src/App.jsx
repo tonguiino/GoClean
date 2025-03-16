db891138d24b7f478b01b74b3fcbd3a36d2c8e8b
tonguiino <santiagotq2@gmail.com>
2025-03-16 13:58:49 -0500

    Add news librarys

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
+
 
 function App() {
   const [count, setCount] = useState(0)
+  
 
   return (
     <>
