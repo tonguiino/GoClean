034c8851e0bd4c6e03f38c4f6559f53c9164e043
tonguiino <santiagotq2@gmail.com>
2025-03-28 12:15:34 -0500

    adiciones de estilos para el home

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -8,8 +8,12 @@ const Home = () => {
         <>
             <Menu />
             <section>
-                <div>
+                <div className="container-img-banner">
                     <img src={banner} className="img-banner" alt="invitacion a ser socio" />
+                </div>
+                <div className="container-goclean-info">
+                    <h2>¿Por qué elegir GoClean?</h2>
+                    <p>No importa si vives en un estudio, casa o tienes una oficina grande, GoClean ofrece soluciones de limpieza personalizadas. Nuestros profesionales de la limpieza son expertos en hacer que tus espacios luzcan siempre relucinetes y lo mejor!! Puedes reservar este servicio en linea y en menos de 1 minuto</p>
                 </div>
 
             </section>
