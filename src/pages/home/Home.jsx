92cc055a6ed631718e016e3bf0f7601a5a727ede
tonguiino <santiagotq2@gmail.com>
2025-03-28 12:46:36 -0500

    se añadieron textos y contenido para home

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -11,9 +11,34 @@ const Home = () => {
                 <div className="container-img-banner">
                     <img src={banner} className="img-banner" alt="invitacion a ser socio" />
                 </div>
-                <div className="container-goclean-info">
+                <div className="container-goclean-text">
                     <h2>¿Por qué elegir GoClean?</h2>
                     <p>No importa si vives en un estudio, casa o tienes una oficina grande, GoClean ofrece soluciones de limpieza personalizadas. Nuestros profesionales de la limpieza son expertos en hacer que tus espacios luzcan siempre relucinetes y lo mejor!! Puedes reservar este servicio en linea y en menos de 1 minuto</p>
+                </div>
+                <div className="container-goclean-step">
+                    <h2>Obtén un espacio limpio en 3 sencillos pasos</h2>
+                    <p>Reserva un servicio de limpieza</p>
+                    <p>Deja que nuestros profesionales hagan su magia</p>
+                    <p>Disfruta de tu espacio impecable</p>
+                </div>
+                <div className="contaeiner-goclean-questions">
+                    <details name="goclean">
+                        <summary >¿Cuanto cuesta un servicio de limpieza?</summary>
+                        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odio similique assumenda rem iure hic! Suscipit, obcaecati deserunt quae quia non dignissimos et perspiciatis quidem repellat mollitia soluta sit sint alias!</p>
+                    </details>
+                    <details name="goclean">
+                        <summary >¿Cuanto cuesta un servicio de limpieza?</summary>
+                        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odio similique assumenda rem iure hic! Suscipit, obcaecati deserunt quae quia non dignissimos et perspiciatis quidem repellat mollitia soluta sit sint alias!</p>
+                    </details>
+                    <details name="goclean">
+                        <summary >¿Cuanto cuesta un servicio de limpieza?</summary>
+                        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odio similique assumenda rem iure hic! Suscipit, obcaecati deserunt quae quia non dignissimos et perspiciatis quidem repellat mollitia soluta sit sint alias!</p>
+                    </details>
+                </div>
+                <div className="container-goclean-contac">
+                <h2>Contactanos</h2>
+                    <p>No importa si vives en un estudio, casa o tienes una oficina grande, GoClean ofrece soluciones de limpieza personalizadas. Nuestros profesionales de la limpieza son expertos en hacer que tus espacios luzcan siempre relucinetes y lo mejor!! Puedes reservar este servicio en linea y en menos de 1 minuto</p>
+               
                 </div>
 
             </section>
