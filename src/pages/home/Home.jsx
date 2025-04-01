4c7749547dd7d81b85ede5e59b5622305b661780
tonguiino <santiagotq2@gmail.com>
2025-03-31 22:48:27 -0500

    Se añadieron contenido y estilos nuevos para acordeon despleganble

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,6 +1,8 @@
 import Menu from "../../components/header/menu"
 import banner from "../../assets/img/Se_Un_Socio.jpg"
 import "./Home.scss"
+import { IoIosArrowForward } from "react-icons/io";
+
 
 
 const Home = () => {
@@ -23,16 +25,32 @@ const Home = () => {
                 </div>
                 <div className="contaeiner-goclean-questions">
                     <details name="goclean">
-                        <summary >¿Cuanto cuesta un servicio de limpieza?</summary>
-                        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odio similique assumenda rem iure hic! Suscipit, obcaecati deserunt quae quia non dignissimos et perspiciatis quidem repellat mollitia soluta sit sint alias!</p>
+                        <summary >¿Que incluye un servicio de limpieza?</summary>
+                        <p>Un servicio de limpieza cubre la limpieza general de los espacios acordados, como habitaciones, baños, cocina y áreas comunes. Dependiendo del plan elegido, puede incluir barrido, trapeado, aspirado, desinfección de superficies y organización básica.</p>
                     </details>
                     <details name="goclean">
-                        <summary >¿Cuanto cuesta un servicio de limpieza?</summary>
-                        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odio similique assumenda rem iure hic! Suscipit, obcaecati deserunt quae quia non dignissimos et perspiciatis quidem repellat mollitia soluta sit sint alias!</p>
+                        <summary >¿Que pasa si no estoy satisfecho con el servicio?</summary>
+                        <p>Queremos que estés completamente satisfecho. Si el servicio no cumple tus expectativas, puedes reportarlo en nuestra plataforma dentro de las primeras 24 horas. Evaluaremos tu caso y podremos ofrecerte una corrección gratuita o un reembolso parcial, según corresponda.</p>
                     </details>
                     <details name="goclean">
-                        <summary >¿Cuanto cuesta un servicio de limpieza?</summary>
-                        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odio similique assumenda rem iure hic! Suscipit, obcaecati deserunt quae quia non dignissimos et perspiciatis quidem repellat mollitia soluta sit sint alias!</p>
+                        <summary >¿Necesito proporcionar los productos de limpieza?</summary>
+                        <p>No es obligatorio. Nuestros socios llevan sus propios productos y herramientas. Sin embargo, si prefieres que usen productos específicos, puedes coordinarlo con el socio antes del servicio.</p>
+                    </details>
+                    <details name="goclean">
+                        <summary >¿Puedo cambiar la fecha de mi reserva?</summary>
+                        <p>Sí, entendemos que los planes pueden cambiar. Puedes modificar la fecha de tu servicio a través de la plataforma, siempre que lo hagas con al menos 24 horas de anticipación.</p>
+                    </details>
+                    <details name="goclean">
+                        <summary >¿Como puedo cancelar o reprogramar mi reserva?</summary>
+                        <p>Desde la sección de reservas en nuestra plataforma, puedes cancelar o reprogramar tu servicio. Si cancelas con al menos 24 horas de antelación, recibirás un reembolso total. Para cancelaciones con menos tiempo, pueden aplicarse cargos según nuestras políticas.
+
+
+
+
+
+
+
+</p>
                     </details>
                 </div>
                 <div className="container-goclean-contac">
