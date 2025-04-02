c4d0ae1ec024aa5c6e5498b17f6eb638af056dac
tonguiino <santiagotq2@gmail.com>
2025-04-01 21:04:45 -0500

    Se cambio el padding de app, se agregaron estilos a footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -10,8 +10,12 @@ const Footer = () => {
     return (
         <>
             <footer>
-
-                <p>&copy; {year} GoCLean. Todos lo redechos reservados. </p>
+                <div className='footer'>
+                    <p>Terminos de servicio</p>
+                    <p>Politica de privacidad</p>
+                    <p>Mapa del sitio</p>
+                    <p className='middle'>&copy; {year} GoCLean. Todos lo redechos reservados. </p>
+                </div>
             </footer>
         </>
     )
