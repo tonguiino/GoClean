5f1776e0e1ce75c2af113b2d4bb6a3cd40af039d
tonguiino <santiagotq2@gmail.com>
2025-04-02 13:33:56 -0500

    Se implementaros las card a vista precios

--- src/pages/precio/Precio.jsx
+++ src/pages/precio/Precio.jsx
@@ -9,15 +9,16 @@ const Precio = () => {
         <>
             <Menu />
             <section className="price-container">
-                {/* <h2>Elige el plan perfecto para ti</h2>
-                <h3>Nuestros servicios están diseñados para adaptarse a tu tiempo y necesidades</h3> */}
+                <h2>Elige el plan perfecto para ti</h2>
+                <h3>Nuestros servicios están diseñados para adaptarse a tu tiempo y necesidades</h3>
 
+                <div className="cards-container">
+                    <Card className="card" titulo="Plan mañana" hora_inicio="8:00 a.m" hora_final="12:00 m" tiempo="4" precio="80.000" ></Card>
 
-                <Card titulo="Plan mañana" hora_inicio="8:00 a.m" hora_final="12:00 m" tiempo="4" precio="80.000" ></Card>
+                    <Card titulo="Plan Tarde" hora_inicio="1:00 p.m" hora_final="5:00 p.m" tiempo="4" precio="80.000"></Card>
 
-                <Card titulo="Perro viejo" hora_inicio="12:00 m" hora_final="4:00 p.m"></Card>
-
-                <Card></Card>
+                    <Card titulo="Plan día completo" hora_inicio="8:00 a.m" hora_final="4:00 p.m" tiempo="8" precio="140.000"></Card>
+                </div>
             </section>
             <Footer />
 
