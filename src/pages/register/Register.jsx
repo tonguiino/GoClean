2d8a345e1c54d3c414f40bb1b57c4c4146daca7a
tonguiino <santiagotq2@gmail.com>
2025-04-04 18:28:32 -0500

    se creo componente nuevo para registro y se crea formulario

---
+++ src/pages/register/Register.jsx
@@ -0,0 +1,39 @@
+import "./Register.scss"
+
+const Register = () => {
+
+
+    return (
+        <>
+
+
+            <form action="">
+                <label htmlFor="nombre">Nombre</label>
+                <input type="text" placeholder="nombre" />
+                <label htmlFor="mail">Correo electronico</label>
+                <input type="email" />
+                <label htmlFor="telefono">Telefono</label>
+                <input type="text" />
+                <label htmlFor="direccion">Direccion</label>
+                <input type="text" />
+                <label htmlFor="contraseña">Contraseña</label>
+                <input type="password" />
+                <label htmlFor="confirmar-contraseña">Confirmar contraseña</label>
+                <input type="password" />
+                <label htmlFor="sexo">sexo</label>
+                <select>
+                    <option value="h">Hombre</option>
+                    <option value="m">Mujer</option>
+                    <option value="o">otro</option>
+                </select>
+                <label htmlFor="rol">registrate como:</label>
+                <select name="" id="">
+                    <option value="u">Usuario</option>
+                    <option value="s">Socio</option>
+                </select>
+
+            </form>
+        </>
+    )
+}
+export default Register;
