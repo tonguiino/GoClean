1ab9272afbf956278792a290d029facb0f745269
tonguiino <santiagotq2@gmail.com>
2025-04-04 19:01:13 -0500

    se incorpora libreria hook form

--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -1,9 +1,10 @@
+import { useForm } from "react-hook-form";
 import Footer from "../../components/footer/Footer";
 import Menu from "../../components/header/menu";
 import "./Register.scss"
 
 const Register = () => {
-
+    const {register,handleSubmit } = useForm();
 
     return (
         <>
@@ -11,25 +12,25 @@ const Register = () => {
             <section className="register-container">
                 <form className="register-form" action="">
                     <label htmlFor="nombre">Nombre</label>
-                    <input type="text" placeholder="nombre" />
-                    <label htmlFor="mail">Correo electronico</label>
-                    <input type="email" />
+                    <input type="text" placeholder="nombre" {...register('nombre')}/>
+                    <label htmlFor="email">Correo electronico</label>
+                    <input type="email" {...register('email')}/>
                     <label htmlFor="telefono">Telefono</label>
-                    <input type="text" />
+                    <input type="text" {...register('telefono')} />
                     <label htmlFor="direccion">Direccion</label>
-                    <input type="text" />
+                    <input type="text" {...register('direccion')} />
                     <label htmlFor="contraseña">Contraseña</label>
-                    <input type="password" />
+                    <input type="password" {...register('contraseña')} />
                     <label htmlFor="confirmar-contraseña">Confirmar contraseña</label>
-                    <input type="password" />
+                    <input type="password" {...register('confirmar-contraseña')} />
                     <label htmlFor="sexo">sexo</label>
-                    <select>
+                    <select {...register('sexo')}>
                         <option value="h">Hombre</option>
                         <option value="m">Mujer</option>
                         <option value="o">otro</option>
                     </select>
                     <label htmlFor="rol">registrate como:</label>
-                    <select name="" id="">
+                    <select {...register('rol')}>
                         <option value="u">Usuario</option>
                         <option value="s">Socio</option>
                     </select>
